d38b7fdd0031cf157095ac2af89c2d9a51667e37
Oleh Kir <oleggio.kir@gmail.com>
2022-06-04 16:59:13 +0300

    Refactored game logic

---
+++ src/main/scala/org/helgi/tetris/game/GameState.scala
@@ -0,0 +1,100 @@
+package org.helgi.tetris.game
+
+import org.helgi.tetris.game.GameStatus.Active
+import org.helgi.tetris.game.Pos.*
+
+import scala.annotation.tailrec
+import scala.util.Random
+
+type TetrominoProducer = LazyList[TetrominoType]
+
+// TODO integrate into GameState
+def randomStream(random: Random): TetrominoProducer =
+  TetrominoType(random.nextInt(TetrominoType.typesNr)) #:: randomStream(random)
+
+enum GameStatus:
+  case Active, Over
+
+case class GameState(gridSize: (Int, Int), placedBlocks: Set[Pos],
+                     currentPiece: Piece, next: TetrominoType,
+                     held: Option[TetrominoType] = None, canHold: Boolean = true,
+                     status: GameStatus = Active):
+  private lazy val rng = new Random()
+
+  private lazy val startPlace: Pos = (gridSize._1 / 2, gridSize._2 - 1)
+
+  def rotate: GameState = makeMove(_.rotate)
+
+  def left: GameState = makeMove(_.left)
+
+  def right: GameState = makeMove(_.right)
+
+  def tick: GameState = makeMove(_.down, { _ => afterDropState })
+
+  def drop: GameState =
+    move(_.down)
+      .map(_.drop)
+      .getOrElse(afterDropState)
+
+  def hold: GameState =
+    if canHold then
+      held.orElse(Some(next))
+        .map(Piece(startPlace, _))
+        .flatMap(tryToPlace(placedBlocks, _))
+        .map { cp =>
+          val hp = Some(currentPiece.kind)
+          val np = if held.isEmpty then randomPiece else next
+          copy(currentPiece = cp, next = np, held = hp, canHold = false)
+        }.getOrElse(this)
+    else this
+
+  private def afterDropState: GameState =
+    tryToGenerateNewPiece.map {
+      GameState(gridSize, placedBlocks ++ currentPiece.blocks,
+        _, randomPiece, held)
+    }.getOrElse(copy(status = GameStatus.Over))
+
+  @tailrec
+  private def tryToPlace(blocks: Set[Pos], p: Piece): Option[Piece] =
+    if !inBound(p.pos) then None
+    else if !isColliding(blocks, p) then Some(p)
+    else tryToPlace(blocks, p.copy(pos = p.pos + (0, 1)))
+
+  private def tryToGenerateNewPiece: Option[Piece] =
+    val piece = Piece(startPlace, next)
+    val blocks = placedBlocks ++ currentPiece.blocks
+    tryToPlace(blocks, piece)
+
+  private def randomPiece: TetrominoType = TetrominoType(rng.nextInt(TetrominoType.typesNr))
+
+  private def inBoundOrOverTop(block: Pos): Boolean =
+    block.x >= 1 && block.x <= gridSize._1 && block.y >= 1
+
+  private def inBound(block: Pos): Boolean =
+    inBoundOrOverTop(block) && block.y <= gridSize._2
+
+  private def inBound(p: Piece): Boolean =
+    p.blocks forall inBoundOrOverTop &&
+      p.blocks exists inBound
+
+  private def isColliding(blocks: Set[Pos], block: Pos): Boolean =
+    blocks contains block
+
+  private def isColliding(blocks: Set[Pos], p: Piece): Boolean =
+    p.blocks exists isColliding(blocks, _)
+
+  private def isColliding(p: Piece): Boolean =
+    isColliding(placedBlocks, p)
+
+  private def validatePieceLocation(p: Piece): Boolean =
+    !isColliding(p) && inBound(p)
+
+  private def checkMove(s: GameState): Option[GameState] =
+    if s.validatePieceLocation(s.currentPiece) then Some(s) else None
+
+  private def move(f: Piece => Piece): Option[GameState] = checkMove(copy(currentPiece = f(currentPiece)))
+
+  private def makeMove(f: Piece => Piece, onFail: (=> Piece) => GameState = _ => this): GameState =
+    move(f).getOrElse(onFail(f(currentPiece)))
+
+
---
+++ src/main/scala/org/helgi/tetris/game/Piece.scala
@@ -0,0 +1,105 @@
+package org.helgi.tetris.game
+
+import org.helgi.tetris.game.Pos.*
+
+object Pos:
+  type Pos = (Int, Int)
+
+  extension (p: Pos)
+    def x = p._1
+    def y = p._2
+    def +(o: Pos) = (p.x + o.x, p.y + o.y)
+
+enum TetrominoType:
+  case I, J, L, O, S, T, Z
+
+object TetrominoType:
+  val typesNr: Int = 7
+
+  def apply(n: Int): TetrominoType = n match
+    case 0 => TetrominoType.I
+    case 1 => TetrominoType.J
+    case 2 => TetrominoType.L
+    case 3 => TetrominoType.O
+    case 4 => TetrominoType.S
+    case 5 => TetrominoType.T
+    case 6 => TetrominoType.Z
+
+
+case class Piece(pos: Pos, kind: TetrominoType, theta: Int = 0):
+  require(Set(0, 90, 180, 270).contains(theta))
+
+  def rotate: Piece = rotate(1)
+
+  def rotate(times: Int): Piece = copy(theta = (theta + 90 * times) % 360)
+
+  def right: Piece = right(1)
+
+  def right(times: Int): Piece = moveHorizontally(times)
+
+  def left: Piece = left(1)
+
+  def left(times: Int): Piece = moveHorizontally(-times)
+
+  def moveHorizontally(dx: Int): Piece = copy(pos = (pos.x + dx, pos.y))
+
+  def down: Piece = down(1)
+
+  def down(times: Int): Piece = moveVertically(-times)
+
+  def up: Piece = up(1)
+
+  def up(times: Int): Piece = moveVertically(times)
+
+  def moveVertically(dy: Int): Piece = copy(pos = (pos.x, pos.y + dy))
+
+  lazy val blocks: List[Pos] = (kind match
+    case TetrominoType.I => List((-1, 0), (0, 0), (1, 0), (2, 0))
+    case TetrominoType.J => List((-1, 1), (-1, 0), (0, 0), (1, 0))
+    case TetrominoType.L => List((-1, 0), (0, 0), (1, 0), (1, 1))
+    case TetrominoType.O => List((0, 1), (0, 0), (1, 0), (1, 1))
+    case TetrominoType.S => List((-1, 0), (0, 0), (0, 1), (1, 1))
+    case TetrominoType.T => List((-1, 0), (0, 0), (0, 1), (1, 0))
+    case TetrominoType.Z => List((-1, 1), (0, 1), (0, 0), (1, 0))
+    ).map(rotateBlock).map(_ + pos)
+
+  def rotatePos(pos: Pos): Pos =
+    val c = math.cos(-theta.toDouble.toRadians).round.toInt
+    val s = math.sin(-theta.toDouble.toRadians).round.toInt
+
+    (pos.x * c - pos.y * s, pos.x * s + pos.y * c)
+
+  def rotateBlock(block: Pos): Pos =
+  // handle special cases for O and I tetrominoes
+    if kind == TetrominoType.O then block
+    else if kind == TetrominoType.I then
+      theta match {
+        case 0 => rotatePos(block)
+        case 90 => rotatePos(block) + (1, 0)
+        case 180 => rotatePos(block) + (1, -1)
+        case 270 => rotatePos(block) + (0, -1)
+      }
+    else
+      rotatePos(block)
+
+
+object Test:
+
+  def main(args: Array[String]): Unit =
+    val p = Piece((3, 4), TetrominoType.I)
+
+    println(p.rotate(4) == p)
+
+    println(mkTetrisField(p.blocks) + "\n")
+    println(mkTetrisField(p.rotate.blocks) + "\n")
+    println(mkTetrisField(p.rotate(2).blocks) + "\n")
+    println(mkTetrisField(p.rotate(3).blocks) + "\n")
+    println(mkTetrisField(p.rotate(4).blocks) + "\n")
+
+  def mkTetrisField(ps: Seq[Pos]): String =
+    def mkIthRow(i: Int): String =
+      (for j <- 1 to 10
+        yield if ps.contains((j, i)) then "O" else "-").mkString
+
+    (for i <- 20 to 1 by -1
+      yield mkIthRow(i)).mkString("\n")
